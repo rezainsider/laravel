48409376d926fb7b53455c1732494a05f6b127e3
Erkan Özkök <erkanozkok@gmail.com>
2018-10-02 12:26:28 +0300

    Update AuthController.php

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -8,6 +8,9 @@ use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
+
+
+
 class AuthController extends Controller
 {
     /*
